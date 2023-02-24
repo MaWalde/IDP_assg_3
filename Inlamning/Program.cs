18fb4a48de8102f16f4173bdd9394e973c8e275b
Mattias <walde87@msn.com>
2023-02-24 17:09:58 +0100

    Lagt till elementen syre, järn och guld.

--- Inlamning/Program.cs
+++ Inlamning/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Element syre = new Element() { namn = Syre, Z = 8, typ = ickemetall, smältpunkt = 54.36, kokpunkt = 90.188 };
+            Element syre = new Element() { namn = Syre, Z = 8, typ = Ickemetall, smältpunkt = 54.36, kokpunkt = 90.188 };
+            Element järn = new Element() { namn = Järn, Z = 26, typ = Metall, smältpunkt = 1811, kokpunkt = 3134 };
+            Element guld = new Element() { namn = Guld, Z = 79, typ = Metall, smältpunkt = 1337.33, kokpunkt = 3243 };
         }
         class Element
         {
